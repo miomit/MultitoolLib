b22438c47db16a9a4ee99c6ac28aca177def739c
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-16 21:46:34 +0300

    added InputSize

--- Source/NeoMath/MatrixException.cs
+++ Source/NeoMath/MatrixException.cs
@@ -7,4 +7,6 @@ internal static class MatrixException
     public static void SizesDontMatch() => throw new Exception("The dimensions of the matrix do not match.");
 
     public static void CannotMul() => throw new Exception("Matrices cannot be multiplied.");
+
+    public static void InputSize() => throw new Exception("The entered size does not correspond to reality.");
 }
