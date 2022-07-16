d5ce3ec4eba4b049ae266ab9dbed9b0a2910c787
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-16 14:33:33 +0300

    added CannotMul

--- Source/NeoMath/MatrixException.cs
+++ Source/NeoMath/MatrixException.cs
@@ -5,4 +5,6 @@ internal static class MatrixException
     public static void LineDoesNotExist(bool isRow = true) => throw new Exception($"this {(isRow ? "row" : "column")} of the matrix does not exist.");
 
     public static void SizesDontMatch() => throw new Exception("The dimensions of the matrix do not match.");
+
+    public static void CannotMul() => throw new Exception("Matrices cannot be multiplied.");
 }
