86c8d5076c6c1b0b3ccea55c006859c210e53045
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-17 15:26:55 +0300

    adding missing code blocks and refactoring code

--- Source/TUI/SelectItem.cs
+++ Source/TUI/SelectItem.cs
@@ -2,27 +2,37 @@ namespace TUI;
 
 public class SelectItem<T>
 {
-    public List<T> ItemList;
+    private int PositionLeft, PositionTop;
+
+    private List<T> ItemList;
 
     public int SelectIndex { get; private set; }
 
-    public T Value;
+    public T Value {get; private set; }
 
-    public SelectItem(List<T> itemList)
+    public SelectItem(List<T> itemList, bool isEndl = true)
     {
         SelectIndex = 0;
         ItemList = itemList;
 
-        ConsoleKeyInfo keyInfo = Console.ReadKey();
-        while(keyInfo.Key != ConsoleKey.Enter)
-        {
-            if (keyInfo.Key == ConsoleKey.UpArrow) SelectUp();
-            if (keyInfo.Key == ConsoleKey.DownArrow) SelectDown();
+        PositionLeft = 0;
+        PositionTop = Console.CursorTop;
 
+        Console.SetCursorPosition(0, PositionTop + itemList.Count - 1);
+
+        if (isEndl) Console.Write('\n');
+
+        ConsoleKeyInfo keyInfo;
+        do
+        {
             Draw();
 
             keyInfo = Console.ReadKey();
-        }
+
+            if (keyInfo.Key == ConsoleKey.UpArrow) SelectUp();
+            if (keyInfo.Key == ConsoleKey.DownArrow) SelectDown();
+
+        }while(keyInfo.Key != ConsoleKey.Enter);
 
         Value = ItemList[SelectIndex];
     }
@@ -41,12 +51,18 @@ public class SelectItem<T>
 
     private void Draw()
     {
-        Console.Clear();
+        int oldPositionLeft = Console.CursorLeft;
+        int oldPositionTop = Console.CursorTop;
+
+        Console.SetCursorPosition(PositionLeft, PositionTop);
+
         for (int i = 0; i < ItemList.Count; i++)
         {
             Console.Write($"[{(SelectIndex == i ? "*" : " ")}] ");
             Console.WriteLine(ItemList[i]);
         }
+
+         Console.SetCursorPosition(oldPositionLeft, oldPositionTop);
         
     }
 }
