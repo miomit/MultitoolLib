9274beb29d8aaca18e443a97f6dea1092a751fd5
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-17 15:40:33 +0300

    added Samples SelectItem

---
+++ Samples/TUI/SelectItem/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What should I output?");
+
+TUI.SelectItem<string> sl = new (
+    new List<string>(){
+        "Hello",
+        "Hello World",
+        "How are you",
+        "ohh :D"
+    }
+);
+
+Console.WriteLine(sl.Value);
