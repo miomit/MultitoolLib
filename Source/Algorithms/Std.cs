847353aacd1ef710224b78e9bc2304b36a2d9178
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-20 17:15:08 +0300

    added GetMin and GetIdByMinElem

--- Source/Algorithms/Std.cs
+++ Source/Algorithms/Std.cs
@@ -8,4 +8,16 @@ public static class Std
         a = b;
         b = c;
     }
+
+    public static int GetIdByMinElem<T>(T[] arr)
+    {
+        //Todo Exception arr == null
+        int id = 0;
+
+        for (int i = 0; i < arr.Length; i++) if (Convert.ToDouble(arr[i]) < Convert.ToDouble(arr[id])) id = i;
+
+        return id;
+    }
+
+    public static T GetMinElem<T>(T[] arr) => arr[GetIdByMinElem<T>(arr)];
 }
