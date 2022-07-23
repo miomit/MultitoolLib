3b34b097a56f98e785561f74791f708439385c9a
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-23 17:30:07 +0300

    added tests for Nod and for Nok

--- Test/Algorithms/Std.cs
+++ Test/Algorithms/Std.cs
@@ -33,4 +33,30 @@ public class StdTest
     }
 
     //TODO test GetMinElem
+
+    [Fact]
+    public void Nod()
+    {
+        int a1 = 84, b1 = 90, res1 = 6;
+        Assert.True(Std.Nod(a1, b1) == res1);
+
+        int a2 = 15, b2 = 28, res2 = 1;
+        Assert.True(Std.Nod(a2, b2) == res2);
+
+        int a3 = 27, b3 = 9,  res3 = 9;
+        Assert.True(Std.Nod(a3, b3) == res3);
+    }
+
+    [Fact]
+    public void Nok()
+    {
+        int a1 = 126, b1 = 70, res1 = 630;
+        Assert.True(Std.Nok(a1, b1) == res1);
+
+        int a2 = 68, b2 = 34, res2 = 68;
+        Assert.True(Std.Nok(a2, b2) == res2);
+
+        int a3 = 441, b3 = 700,  res3 = 44100;
+        Assert.True(Std.Nok(a3, b3) == res3);
+    }
 }
