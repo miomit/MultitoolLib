836ddb491e8dfbed03deacc143803d5b35b49bb6
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-07-24 13:45:46 +0300

    mv MatrixException and rename

--- Source/Interrupts/Matrix.cs
+++ Source/Interrupts/Matrix.cs
@@ -1,6 +1,6 @@
-namespace NeoMath;
+namespace Interrupts;
 
-internal static class MatrixException
+internal static class Matrix
 {
     public static void LineDoesNotExist(bool isRow = true) => throw new Exception($"this {(isRow ? "row" : "column")} of the matrix does not exist.");
 
