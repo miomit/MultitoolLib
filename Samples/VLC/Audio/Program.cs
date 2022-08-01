f6823e4dcaa32272899702ca3bd8475908b228f0
yuraMovsesyan <movsesyan-03@inbox.ru>
2022-08-01 20:51:30 +0300

    init Audio

---
+++ Samples/VLC/Audio/Program.cs
@@ -0,0 +1,16 @@
+﻿using VLC;
+
+Audio a = new Audio("Assets/asia.mp3");
+
+a.Play();
+
+while (true)
+{
+    var key = Console.ReadKey();
+
+    if (key.KeyChar == 'p') a.Pause();
+
+    if (key.KeyChar == 'r') a.Play();
+
+    if (key.KeyChar == 'q') break;
+}
